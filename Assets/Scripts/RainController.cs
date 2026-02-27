6568ccf09f80c528c0ac6f8ee60be8081617f930
Pradyum22 <73785093+Pradyum22@users.noreply.github.com>
2026-02-27 14:29:23 -0600

    test

--- Assets/Scripts/RainController.cs
+++ Assets/Scripts/RainController.cs
@@ -35,7 +35,7 @@ public class RainController : MonoBehaviour
                 break;
 
             case 1: // Just enough
-                ApplyRainSettings(65f, 10f, 0.465f);
+                ApplyRainSettings(65f, 10f, 0.45f);
                 break;
 
             case 2: // Too much rain
