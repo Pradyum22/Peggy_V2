6dcd79b96c6f9e51863687c96febf5079830c2ce
Pradyum22 <73785093+Pradyum22@users.noreply.github.com>
2026-03-18 19:59:35 -0500

    fire scene

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -2,42 +2,71 @@ using UnityEngine;
 
 public class FireController : MonoBehaviour
 {
-    public ParticleSystem fireLow;
-    public ParticleSystem fireMedium;
-    public ParticleSystem fireHigh;
+    [Header("Fire Particle Systems (all layers)")]
+    public ParticleSystem[] fireSystems;
+
+    private float[] baseEmissionRates;
+    private Vector3 baseScale;
+    private Vector3 targetScale;
+
+    void Start()
+    {
+        // Cache original emission values
+        baseEmissionRates = new float[fireSystems.Length];
+
+        for (int i = 0; i < fireSystems.Length; i++)
+        {
+            baseEmissionRates[i] = fireSystems[i].emission.rateOverTime.constant;
+        }
+
+        // Cache initial scale
+        baseScale = transform.localScale;
+        targetScale = baseScale;
+    }
 
     public void UpdateFire(float fireValue)
     {
+        float emissionMultiplier;
+        float scaleMultiplier;
+
+        // Map slider values (-1, 0, 1)
         if (fireValue == -1)
         {
-            PlayOnly(fireLow);
+            emissionMultiplier = 0.3f;
+            scaleMultiplier = 0.95f;
         }
         else if (fireValue == 0)
         {
-            PlayOnly(fireMedium);
+            emissionMultiplier = 1.0f;
+            scaleMultiplier = 1.0f;
         }
-        else if (fireValue == 1)
+        else // fireValue == 1
         {
-            PlayOnly(fireHigh);
+            emissionMultiplier = 2.0f;
+            scaleMultiplier = 1.1f;
         }
+
+        // Apply emission changes instantly
+        for (int i = 0; i < fireSystems.Length; i++)
+        {
+            var emission = fireSystems[i].emission;
+            emission.rateOverTime = baseEmissionRates[i] * emissionMultiplier;
+
+            if (!fireSystems[i].isPlaying)
+                fireSystems[i].Play();
+        }
+
+        // Set target scale (DO NOT apply directly)
+        targetScale = baseScale * scaleMultiplier;
     }
 
-    void PlayOnly(ParticleSystem activeSystem)
+    void Update()
     {
-        ParticleSystem[] systems = { fireLow, fireMedium, fireHigh };
-
-        foreach (ParticleSystem ps in systems)
-        {
-            if (ps == activeSystem)
-            {
-                if (!ps.isPlaying)
-                    ps.Play();
-            }
-            else
-            {
-                if (ps.isPlaying)
-                    ps.Stop();
-            }
-        }
+        // Smoothly interpolate scale
+        transform.localScale = Vector3.Lerp(
+            transform.localScale,
+            targetScale,
+            Time.deltaTime * 3f   // smoothing speed (tweakable)
+        );
     }
 }
