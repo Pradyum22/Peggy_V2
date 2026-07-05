bd89c318e992cd35c6dd0d2c7fed8e0051fc5daf
kowkow315 <kelly.r.luxton@gmail.com>
2026-07-05 16:51:07 -0700

    burn logic

---
+++ Assets/Scripts/plant scripts/native_burn.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class native_burn : MonoBehaviour
+{
+    private Animator animator;
+    public Button burnCycle;
+    public Object plant;
+
+    private int state = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError($"[roots_static] No Animator found on {name}");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //deactivating after burn for regrow
+        /* if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("isdead"))
+         {
+             if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("isdead"))
+             {
+                 gameObject.SetActive(false);
+             }
+         }
+        */
+        
+
+    }
+
+    void actions()
+    {
+    }
+
+    void OnEnable()
+    {
+        // Add a listener to detect the click event
+        burnCycle.onClick.AddListener(OnButtonClicked);
+    }
+
+    void OnDisable()
+    {
+        // Always remove listeners when disabled to prevent memory leaks
+        burnCycle.onClick.RemoveListener(OnButtonClicked);
+    }
+
+    //button press causing next step of the cycle and reseting
+    private void OnButtonClicked()
+    {
+        state++;
+        if (state > 4)
+        {
+            Debug.Log("cycle reset!");
+            state = 1;
+        }
+        cycleChange();
+        
+    }
+    private void cycleChange()
+    {
+        //activating animation triggers depending on state of the cycle
+        if (state == 2)
+        {
+           
+            Debug.Log("Invade activated");
+            animator.SetTrigger("Invade");
+        }
+        else if (state == 3)
+        {
+            Debug.Log("Burn activated");
+            animator.SetTrigger("Burn");
+        }
+        else if (state == 4)
+        {
+            Debug.Log("Regrow activated");
+            animator.SetTrigger("Regrow");
+        }
+        if (state > 4)
+        {
+            Debug.Log("Return activated");
+            animator.SetTrigger("Return");
+        }
+    }
+}
